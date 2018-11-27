a9172180cf7a01d44cf6dcfcca40ea120831bb0b
lichenguang <lichenguang@hotmail.com>
2018-11-27 14:59:51 +0800

    commit add apscheduler version

---
+++ scheduler.py
@@ -0,0 +1,22 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.executors.pool import ProcessPoolExecutor
+
+from sent_email import send
+
+if __name__ == '__main__':
+	sched()
+    executors = {
+        'default': {'type': 'threadpool', 'max_workers': 20},
+        'processpool': ProcessPoolExecutor(max_workers=5)
+    }
+    job_defaults = {
+        'coalesce': False,
+        'max_instances': 3
+    }
+    sched = BlockingScheduler()
+    sched.configure(executors=executors, job_defaults=job_defaults)
+    sched.add_job(send, 'cron', hour=7, minute=30, misfire_grace_time=200)
+    sched.add_job(send, 'cron', hour=9, minute=10, misfire_grace_time=200)
+    sched.add_job(send, 'cron', hour=13, minute=0, misfire_grace_time=200)
+    sched.add_job(send, 'cron', hour=19, minute=0, misfire_grace_time=200)
+    sched.start()
--- sent_email.py
+++ sent_email.py
@@ -2,27 +2,28 @@ import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
 
-sender = '345657803@qq.com'
-receivers = ['345657803@qq.com']
 
-content = '''
-Please Start Your Scheduler!!!
-book & write
-sport & studing
-video & friend
-english & exercise
-clear
-'''
+def send():
+    sender = '345657803@qq.com'
+    receivers = ['345657803@qq.com']
 
-message = MIMEText(content, 'plain', 'utf-8')
-message['From'] = formataddr(["Scheduler Self", sender])
-message['To'] = formataddr(["My Self", sender])
-subject = 'Day Scheduler'
-message['Subject'] = subject
+    content = '''
+    Please Start Your Scheduler!!!
+    book & write
+    sport & studing
+    video & friend
+    english & exercise
+    clear
+    '''
 
-server = smtplib.SMTP_SSL("smtp.qq.com", 465)
-server.login(sender, 'lzeszgpxyxtpbihi')
-server.sendmail(sender, receivers, message.as_string())
-server.quit()
-print("邮件发送成功")
+    message = MIMEText(content, 'plain', 'utf-8')
+    message['From'] = formataddr(["Scheduler Self", sender])
+    message['To'] = formataddr(["My Self", sender])
+    subject = 'Day Scheduler'
+    message['Subject'] = subject
 
+    server = smtplib.SMTP_SSL("smtp.qq.com", 465)
+    server.login(sender, 'lzeszgpxyxtpbihi')
+    server.sendmail(sender, receivers, message.as_string())
+    server.quit()
+    print("邮件发送成功")
