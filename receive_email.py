70211ea9da177af40ad48806a1501955d08c953e
lichenguang <lichenguang@hotmail.com>
2018-11-27 17:25:08 +0800

    commit optimized receive email python file

--- receive_email.py
+++ receive_email.py
@@ -1,63 +1,60 @@
+import json
 import poplib
 import base64
+import requests
+
 from email import parser
 
-sender = '345657803@qq.com'
-_pass = 'lzeszgpxyxtpbihi'
-pop_conn = poplib.POP3_SSL('pop.qq.com', port=995)
-pop_conn.user(sender)
-pop_conn.pass_(_pass)
-ret = pop_conn.stat()
+SENDER = '345657803@qq.com'
+PASS = 'lzeszgpxyxtpbihi'
 
-today_indexes = []
 
-index_list = list(range(1, ret[0] + 1))[::-1]
-break_flag = False
-for index, i in enumerate(index_list):
-    if break_flag:
-        break
-    top_msg = pop_conn.top(i, 0)
-    msgs = top_msg[1]
-    print(index)
-    subject_flag, from_flag, date_flag = False, False, False
-    for msg in msgs:
-        try:
-            _msg = msg.decode()
-        except UnicodeDecodeError as e:
-            continue
-        except Exception as e:
-            raise
-        if 'Subject:' in _msg:
-            if 'Re: Day Scheduler' in _msg:
-                subject_flag = True
-            else:
-                break
-        if 'From: ' in _msg:
-            if '345657803@qq.com' in _msg:
-                from_flag = True
-            else:
-                break
-        if 'Date:' in _msg:
-            if '27 Nov' in _msg:
-                date_flag = True
-            else:
-                break_flag = True
-                break
-    if subject_flag and from_flag and date_flag:
-        today_indexes.append(index)
+def take_index(pop_conn):
+    msg_indexes = []
+    ret = pop_conn.stat()
+    index_list = list(range(1, ret[0] + 1))[::-1]
+    break_flag = False
+    for index, i in enumerate(index_list):
+        if break_flag:
+            break
+        top_msg = pop_conn.top(i, 0)
+        msgs = top_msg[1]
+        print(index)
+        subject_flag, from_flag, date_flag = False, False, False
+        for msg in msgs:
+            try:
+                _msg = msg.decode()
+            except UnicodeDecodeError as e:
+                continue
+            except Exception as e:
+                raise
+            if 'Subject:' in _msg:
+                if 'Re: Day Scheduler' in _msg:
+                    subject_flag = True
+                else:
+                    break
+            if 'From: ' in _msg:
+                if SENDER in _msg:
+                    from_flag = True
+                else:
+                    break
+            if 'Date:' in _msg:
+                if '27 Nov' in _msg:
+                    date_flag = True
+                else:
+                    break
+        if subject_flag and from_flag and date_flag:
+            break_flag = True
+            msg_indexes.append(len(index_list) - index)
+    return msg_indexes
 
-print('-' * 100)
-print(today_indexes)
-print('-' * 100)
 
-for index in today_indexes:
+def parse_email(index, pop_conn):
     content = ''
     content_flag = False
-    index = len(index_list) - index
     down = pop_conn.retr(index)
     start_content = 'Content-Transfer-Encoding: base64'
     for index, line in enumerate(down[1]):
-        print(line)
         if start_content in line.decode():
             content_flag = True
             continue
@@ -67,6 +64,9 @@ for index in today_indexes:
         if content_flag:
             content += line.decode()
     content = base64.b64decode(content).decode('gbk')
+    content_list = content.split('\n')
+    robot_message = take_robot(content_list[1])
+
     if 'OVER scheduler' in content:
         pass
     elif 'REST scheduler' in content:
@@ -74,4 +74,40 @@ for index in today_indexes:
     elif 'FAIL scheduler' in content:
         pass
     print(content)
-raise Exception('', '')
+    print(robot_message)
+
+
+def take_robot(content):
+    result = ''
+    data = {
+        "reqType": 0,
+        "perception": {
+            "inputText": {
+                "text": content
+            }
+        },
+        "userInfo": {
+            "apiKey": "b903c4228a2247bc934c8d92e510c26c",
+            "userId": "Alben"
+        }
+    }
+    url = 'http://openapi.tuling123.com/openapi/api/v2'
+    r = requests.post(url, json=data)
+    message = json.loads(r.text)
+    for item in message['results']:
+        result += item['values']['text'] + '\r\n'
+    return result
+
+
+def receive():
+    pop_conn = poplib.POP3_SSL('pop.qq.com', port=995)
+    pop_conn.user(SENDER)
+    pop_conn.pass_(PASS)
+
+    msg_indexes = take_index(pop_conn)
+
+    print('-' * 100)
+    print(msg_indexes)
+    print('-' * 100)
+
+    for index in today_indexes:
