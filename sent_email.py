fb0e78bb39eef6fb566d2e322e674aef7698663c
lichenguang <lichenguang@hotmail.com>
2018-12-29 15:49:19 +0800

    add sercer chain sen wechart message info function refs #111

--- sent_email.py
+++ sent_email.py
@@ -1,10 +1,19 @@
 import smtplib
+import requests
 from email.mime.text import MIMEText
 from email.utils import formataddr
 
 sender = '345657803@qq.com'
 receivers = ['345657803@qq.com']
 PASS = 'lzeszgpxyxtpbihi'
+
+
+def server_chan(title, content):
+    url = 'https://sc.ftqq.com/SCU38425T5d31de7e4df59fb4659595571aa736765c271faa316e9.send'
+    data = {'text': title, 'desp': content}
+    r = requests.post(url, data=data)
+    print(r.status_code)
+    pass
 
 
 def send(content=None, _receivers=None):
@@ -31,4 +40,6 @@ def send(content=None, _receivers=None):
     server.login(sender, PASS)
     server.sendmail(sender, _receivers, message.as_string())
     server.quit()
+
+    server_chan(subject, content)
     print("邮件发送成功")
